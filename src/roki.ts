a3459ec727688580705c196042099a0fdc86a416
Kazuki Sawada <kazuki@6715.jp>
2020-09-23 14:21:46 +0900

    impl basic api

--- src/roki.ts
+++ src/roki.ts
@@ -1,5 +1,9 @@
 import { Filesystem } from "./fs";
 
+import path from "path";
+import crypto from "crypto";
+import yaml from "js-yaml";
+
 export default class Roki {
 	private src: Filesystem;
 	private dst: Filesystem;
@@ -9,11 +13,21 @@ export default class Roki {
 		this.dst = dst;
 	}
 
-	async newRevision(path: string, content: string) {
-		return this.src.writeFile(path, Buffer.from(content));
+	async newRevision(page: string, content: string) {
+		const timestamp = new Date();
+		const meta = {
+			page,
+			timestamp,
+			id: timestamp.getTime(),
+		};
+
+		const filePath = path.join(page, "_revision", `${meta.id}.md`);
+		return this.src.writeFile(filePath, Buffer.from(["---", yaml.safeDump(meta).trim(), "---", content.trim()].join("\n")));
 	}
-	async newAttachment(path: string, content: Buffer) {
-		return this.src.writeFile(path, content);
+	async newAttachment(page: string, original: string, content: Buffer) {
+		const id = crypto.createHash("md5").update(content).digest("hex");
+		const filePath = path.join(page, "_attachment", `${id}${path.extname(original)}`);
+		return this.src.writeFile(filePath, content);
 	}
 
 }
