5aaae36393e1e863ef34532e55723ea430e49cf8
Kazuki Sawada <kazuki@6715.jp>
2020-09-24 02:46:27 +0900

    impl delete api

--- src/roki/index.ts
+++ src/roki/index.ts
@@ -13,21 +13,44 @@ export default class Roki {
 		this.dst = dst;
 	}
 
+	private getPagePath(page: string): string {
+		return path.resolve("/", page).substr(1);
+	}
+	private getRevisionPath(page: string, id: string): string {
+		return path.join(this.getPagePath(page), "_revision", `${id}.md`);
+	}
+	private getAttachmentPath(page: string, filename: string): string {
+		return path.join(this.getPagePath(page), "_attachment", filename);
+	}
+
 	async newRevision(page: string, content: string) {
 		const timestamp = new Date();
 		const meta = {
 			page,
 			timestamp,
-			id: timestamp.getTime(),
+			id: timestamp.getTime().toString(36),
 		};
-
-		const filePath = path.join(page, "_revision", `${meta.id}.md`);
-		return this.src.writeFile(filePath, Buffer.from(["---", yaml.safeDump(meta).trim(), "---", content.trim()].join("\n")));
+		return this.src.writeFile(
+			this.getRevisionPath(page, meta.id),
+			Buffer.from(["---", yaml.safeDump(meta).trim(), "---", content.trim()].join("\n")),
+		);
 	}
 	async newAttachment(page: string, original: string, content: Buffer) {
-		const id = crypto.createHash("md5").update(content).digest("hex");
-		const filePath = path.join(page, "_attachment", `${id}${path.extname(original)}`);
-		return this.src.writeFile(filePath, content);
+		const filename = crypto.createHash("md5").update(content).digest("hex") + path.extname(original);
+		return this.src.writeFile(this.getAttachmentPath(page, filename), content);
+	}
+
+	async deletePage(page: string) {
+		return Promise.all([
+			this.src.writeFile(path.dirname(this.getRevisionPath(page, "dummy")), undefined),
+			this.src.writeFile(path.dirname(this.getAttachmentPath(page, "dummy")), undefined),
+		]);
+	}
+	async deleteRevision(page: string, id: string) {
+		return this.src.writeFile(this.getRevisionPath(page, id), undefined);
+	}
+	async deleteAttachment(page: string, filename: string) {
+		return this.src.writeFile(this.getAttachmentPath(page, filename), undefined);
 	}
 
 }
